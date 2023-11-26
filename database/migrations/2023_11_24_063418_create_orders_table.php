1ef1fc7093880d3f953614153b8e1d390a5c7206
alifadill <funies32@gmail.com>
2023-11-26 12:37:53 +0700

    Fix migration fk orders table

--- database/migrations/2023_11_24_063418_create_orders_table.php
+++ database/migrations/2023_11_24_063418_create_orders_table.php
@@ -16,15 +16,10 @@ return new class extends Migration
             $table->string('address');
             $table->string('subdistrict');
             $table->string('city');
-            $table->string('status_id');
-            $table->string('user_id');
-            $table->string('paket_id');
-            $table->string('teknisi_id');
-
-            $table->foreign('status_id')->references('id')->on('statuses');
-            $table->foreign('user_id')->references('id')->on('users');
-            $table->foreign('teknisi_id')->references('id')->on('teknisis');
-            $table->foreign('paket_id')->references('id')->on('pakets');         
+            $table->unsignedBigInteger('status_id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('paket_id');
+            $table->unsignedBigInteger('teknisi_id');
             $table->timestamps();
         });
     }
